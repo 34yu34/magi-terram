0b44ed24a65d97b8a2248c34a0a5a5649bb1b193
Billy <billy.bouchard03@gmail.com>
2022-05-23 13:57:05 -0400

    push

---
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using Scripts.Units;
+using UnityEngine;
+
+namespace Scripts.Projectile
+{
+    public class Projectile : MonoBehaviour
+    { 
+        
+        public Unit Target { get; private set; }
+        public Unit Launcher { get; private set; }
+        
+        public delegate void OnHit(Unit target);
+
+        private OnHit _on_target_hit_event;
+        
+        private void Update()
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/Units/UnitStates/AttackState.cs
+++ Assets/Scripts/Units/UnitStates/AttackState.cs
@@ -6,21 +6,31 @@ namespace Scripts.Units.UnitStates
 {
     public class AttackState : AbstractUnitState
     {
-        private Timer _next_attack_time;
+        private Timer _next_attack_timer;
 
         private Stat AttackSpeed => Unit.GetStat(StatsTypes.AttackSpeed);
+
+        private float AttackCooldown => 1f / AttackSpeed.CurrentValue;
 
         public AttackState(Unit unit) : base(unit)
         { }
 
         public override void OnEnter()
         {
-            _next_attack_time = Timer.In(1f / AttackSpeed.CurrentValue);
+            _next_attack_timer = Timer.In(AttackCooldown);
         }
 
         public override void OnUpdate()
         {
-            throw new System.NotImplementedException();
+            if (_next_attack_timer.IsCompleted())
+            {
+                Attack();
+            }
+        }
+
+        private void Attack()
+        {
+            
         }
 
         public override IState NextState()
